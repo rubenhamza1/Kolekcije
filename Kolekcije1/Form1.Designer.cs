cd35cf236a879e969bac4eebc9139394807ab932
rubenhamza1 <90246098+rubenhamza1@users.noreply.github.com>
2022-02-09 21:05:40 +0100

    Add files via upload

---
+++ Kolekcije1/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Kolekcije1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbPodatak1 = new System.Windows.Forms.TextBox();
+            this.txbPodatak2 = new System.Windows.Forms.TextBox();
+            this.txbPodatak3 = new System.Windows.Forms.TextBox();
+            this.txbPodatak4 = new System.Windows.Forms.TextBox();
+            this.btnUnesi = new System.Windows.Forms.Button();
+            this.btnObradi = new System.Windows.Forms.Button();
+            this.btnIspisi = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.lblPodatak1 = new System.Windows.Forms.Label();
+            this.lblPodatak2 = new System.Windows.Forms.Label();
+            this.lblPodatak3 = new System.Windows.Forms.Label();
+            this.lblPodatak4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txbPodatak1
+            // 
+            this.txbPodatak1.Location = new System.Drawing.Point(12, 44);
+            this.txbPodatak1.Name = "txbPodatak1";
+            this.txbPodatak1.Size = new System.Drawing.Size(143, 20);
+            this.txbPodatak1.TabIndex = 0;
+            // 
+            // txbPodatak2
+            // 
+            this.txbPodatak2.Location = new System.Drawing.Point(12, 87);
+            this.txbPodatak2.Name = "txbPodatak2";
+            this.txbPodatak2.Size = new System.Drawing.Size(143, 20);
+            this.txbPodatak2.TabIndex = 1;
+            // 
+            // txbPodatak3
+            // 
+            this.txbPodatak3.Location = new System.Drawing.Point(12, 133);
+            this.txbPodatak3.Name = "txbPodatak3";
+            this.txbPodatak3.Size = new System.Drawing.Size(143, 20);
+            this.txbPodatak3.TabIndex = 2;
+            // 
+            // txbPodatak4
+            // 
+            this.txbPodatak4.Location = new System.Drawing.Point(12, 177);
+            this.txbPodatak4.Name = "txbPodatak4";
+            this.txbPodatak4.Size = new System.Drawing.Size(143, 20);
+            this.txbPodatak4.TabIndex = 3;
+            // 
+            // btnUnesi
+            // 
+            this.btnUnesi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnUnesi.Font = new System.Drawing.Font("MS Gothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnUnesi.Location = new System.Drawing.Point(177, 44);
+            this.btnUnesi.Name = "btnUnesi";
+            this.btnUnesi.Size = new System.Drawing.Size(159, 153);
+            this.btnUnesi.TabIndex = 4;
+            this.btnUnesi.Text = "Unesi";
+            this.btnUnesi.UseVisualStyleBackColor = false;
+            // 
+            // btnObradi
+            // 
+            this.btnObradi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnObradi.Font = new System.Drawing.Font("MS Gothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnObradi.Location = new System.Drawing.Point(351, 44);
+            this.btnObradi.Name = "btnObradi";
+            this.btnObradi.Size = new System.Drawing.Size(157, 153);
+            this.btnObradi.TabIndex = 5;
+            this.btnObradi.Text = "Obradi";
+            this.btnObradi.UseVisualStyleBackColor = false;
+            this.btnObradi.Click += new System.EventHandler(this.btnObradi_Click);
+            // 
+            // btnIspisi
+            // 
+            this.btnIspisi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnIspisi.Font = new System.Drawing.Font("MS Gothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnIspisi.Location = new System.Drawing.Point(524, 44);
+            this.btnIspisi.Name = "btnIspisi";
+            this.btnIspisi.Size = new System.Drawing.Size(149, 153);
+            this.btnIspisi.TabIndex = 6;
+            this.btnIspisi.Text = "Ispisi";
+            this.btnIspisi.UseVisualStyleBackColor = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 212);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(661, 226);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // lblPodatak1
+            // 
+            this.lblPodatak1.AutoSize = true;
+            this.lblPodatak1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblPodatak1.Location = new System.Drawing.Point(9, 28);
+            this.lblPodatak1.Name = "lblPodatak1";
+            this.lblPodatak1.Size = new System.Drawing.Size(59, 13);
+            this.lblPodatak1.TabIndex = 8;
+            this.lblPodatak1.Text = "Podatak 1:";
+            // 
+            // lblPodatak2
+            // 
+            this.lblPodatak2.AutoSize = true;
+            this.lblPodatak2.Location = new System.Drawing.Point(9, 71);
+            this.lblPodatak2.Name = "lblPodatak2";
+            this.lblPodatak2.Size = new System.Drawing.Size(59, 13);
+            this.lblPodatak2.TabIndex = 9;
+            this.lblPodatak2.Text = "Podatak 2:";
+            // 
+            // lblPodatak3
+            // 
+            this.lblPodatak3.AutoSize = true;
+            this.lblPodatak3.Location = new System.Drawing.Point(9, 114);
+            this.lblPodatak3.Name = "lblPodatak3";
+            this.lblPodatak3.Size = new System.Drawing.Size(59, 13);
+            this.lblPodatak3.TabIndex = 10;
+            this.lblPodatak3.Text = "Podatak 3:";
+            // 
+            // lblPodatak4
+            // 
+            this.lblPodatak4.AutoSize = true;
+            this.lblPodatak4.Location = new System.Drawing.Point(12, 160);
+            this.lblPodatak4.Name = "lblPodatak4";
+            this.lblPodatak4.Size = new System.Drawing.Size(59, 13);
+            this.lblPodatak4.TabIndex = 11;
+            this.lblPodatak4.Text = "Podatak 4:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 450);
+            this.Controls.Add(this.lblPodatak4);
+            this.Controls.Add(this.lblPodatak3);
+            this.Controls.Add(this.lblPodatak2);
+            this.Controls.Add(this.lblPodatak1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnIspisi);
+            this.Controls.Add(this.btnObradi);
+            this.Controls.Add(this.btnUnesi);
+            this.Controls.Add(this.txbPodatak4);
+            this.Controls.Add(this.txbPodatak3);
+            this.Controls.Add(this.txbPodatak2);
+            this.Controls.Add(this.txbPodatak1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbPodatak1;
+        private System.Windows.Forms.TextBox txbPodatak2;
+        private System.Windows.Forms.TextBox txbPodatak3;
+        private System.Windows.Forms.TextBox txbPodatak4;
+        private System.Windows.Forms.Button btnUnesi;
+        private System.Windows.Forms.Button btnObradi;
+        private System.Windows.Forms.Button btnIspisi;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label lblPodatak1;
+        private System.Windows.Forms.Label lblPodatak2;
+        private System.Windows.Forms.Label lblPodatak3;
+        private System.Windows.Forms.Label lblPodatak4;
+    }
+}
+
